9d0430f097e265703c991b4fb86dcd367c3b7b3c
Ash <ash@ashryanbeats.com>
2016-07-30 20:16:35 -0400

    Parsing JSON date.

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -14,10 +14,25 @@ enum Method: String {
     case RecentPhotos = "flickr.photos.getRecent"
 }
 
+enum PhotosResult {
+    case Success([Photo])
+    case Failure(ErrorType)
+}
+
+enum FlickrError: ErrorType {
+    case InvalidJSONData
+}
+
 struct FlickrAPI {
     
     private static let baseURLString = "https://api.flickr.com/services/rest"
     private static let APIKey = flickrAPIkey
+    
+    private static let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
     
     private static func flickrURL(method method: Method, parameters: [String:String]?) -> NSURL {
 
@@ -52,4 +67,43 @@ struct FlickrAPI {
         return flickrURL(method: .RecentPhotos, parameters: ["extras": "url_h,date_taken"])
     }
 
+    static func photosFromJSONData(data: NSData) -> PhotosResult {
+        do {
+            let jsonObject: AnyObject = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+            
+            guard let jsonDictionary = jsonObject as? [NSObject:AnyObject], photos = jsonDictionary["photos"] as? [String:AnyObject], photosArray = photos["photo"] as? [[String:AnyObject]] else {
+                return .Failure(FlickrError.InvalidJSONData)
+            }
+            
+            var finalPhotos = [Photo]()
+            for photoJSON in photosArray {
+                if let photo = photoFromJSONObject(photoJSON) {
+                    finalPhotos.append(photo)
+                }
+            }
+            
+            if finalPhotos.count == 0 && photosArray.count > 0 {
+                return .Failure(FlickrError.InvalidJSONData)
+            }
+            
+            return .Success(finalPhotos)
+        }
+        catch let error {
+            return .Failure(error)
+        }
+    }
+    
+    private static func photoFromJSONObject(json: [String : AnyObject]) -> Photo? {
+        guard let
+            photoID = json["id"] as? String,
+            title = json["title"] as? String,
+            dateString = json["datetaken"] as? String,
+            photoURLString = json["url_h"] as? String,
+            url = NSURL(string: photoURLString),
+            dateTaken = dateFormatter.dateFromString(dateString) else {
+                return nil
+        }
+        
+        return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
+    }
 }
